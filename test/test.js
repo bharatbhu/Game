c86b130a7e29459564a7ac6db01a55d9795f885a
bharatbhu <bharatbhu@cybage.com>
2016-07-26 19:09:10 +0530

    test version changes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var should = require('should');
 var io = require('socket.io-client');
-var socketURL = 'http://localhost:3000';
+var socketURL = 'http://localhost:5000';
 
 var options = {
     transports: ['websocket'],
